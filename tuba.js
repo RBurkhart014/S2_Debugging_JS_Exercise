d34941e9f862b4d4e133e8a5e8d495b614e5b097
Ryan Burkhart <RBurkhart014@west-mec.org>
2019-04-25 18:19:41 -0700

    Started Debugging, Fixed Javascript

--- tuba.js
+++ tuba.js
@@ -3,8 +3,8 @@
     Tuba Farm Equipment
  *    Variables and functions
  *    
- *    Author: 
- *    Date:   
+ *    Author: Ryan Burkhart
+ *    Date: 4/25/19  
 
  *    Filename: tuba.js
  */
@@ -13,7 +13,7 @@
 var acresComplete = true;
 var cropsComplete = true;
 var monthsComplete = true;
-ar fuelComplete = true;
+var fuelComplete = true;
 
 /* global variables referencing sidebar h2 and p elements */
 var messageHeadElement = document.getElementById("messageHead");
@@ -22,15 +22,15 @@ var messageElement = document.getElementById("message");
 /* global variables referencing fieldset elements */
 var acresFieldset = document.getElementsByTagName("fieldset")[0];
 var cropsFieldset = document.getElementsByTagName("fieldset")[1];
-var monthsFieldset document.getElementsByTagName("fieldset")[2];
-var fuelFieldset = document.getElementsByTagName("fieldset)[3];
+var monthsFieldset = document.getElementsByTagName("fieldset")[2];
+var fuelFieldset = document.getElementsByTagName("fieldset")[3];
 
     /* global variables referencing text input elements */
     var monthsBox = document.forms[0].months;
     var acresBox = document.forms[0].acres;
 
     /* verify acres text box entry is a positive number */
-    function verifyAcres) {
+    function verifyAcres() {
     testFormCompleteness();
 }
 
